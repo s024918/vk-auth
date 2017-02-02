4041349b7acade47246ed4c59081c14471fa64b9
s024918 <apsivaflines@gmail.com>
2017-02-02 17:01:41 +0200

    Added Login/Registration/Home pages

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app = express()
 
 app.use(express.static('www'))
 
+app.all('/*', function(req, res, next) {
+  res.sendFile('www/index.html', { root: __dirname });
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
