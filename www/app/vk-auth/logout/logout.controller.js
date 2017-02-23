7fd84c08ce493dab0ea69a8d378a69b6570ce5cd
s024918 <apsivaflines@gmail.com>
2017-02-23 20:03:33 +0200

    No Comment

--- www/app/vk-auth/logout/logout.controller.js
+++ www/app/vk-auth/logout/logout.controller.js
@@ -1,22 +1,11 @@
 (function() {
 	'use strict';
 
-	app.controller("LogoutCtrl", ["$rootScope", "$location", "httpService", "localStorageService",
-		function ($rootScope, $location, httpService, localStorageService) {
-			var account = {
-					email: null,
-					password: null
-				};
-				
-			var promisePost = httpService.post("login", account);
-			promisePost.then(function (response) {
-				$rootScope.user = null;
-					localStorageService.set('vk-auth', null);
-					$location.path('/login');
-			},
-			function (error) {
-				alert("nereikia taip daryti...");
-			});
+	app.controller("LogoutCtrl", ["$rootScope", "$location", "localStorageService",
+		function ($rootScope, $location, localStorageService) {
+			$rootScope.user = null;
+			localStorageService.set('vk-auth', null);
+			$location.path('/login');
 		}
 	]);
 })();
